5ef3214e8392a80a8e37f2bad776ec45457e02c7
Haeryz <haeriz42069@gmail.com>
2026-04-03 22:12:43 +0700

    feat(AddTimelineEntryForm): add WhatsAppNotifyToggle component to the form

--- app/[locale]/(routes)/mektek/[id]/_components/AddTimelineEntryForm.tsx
+++ app/[locale]/(routes)/mektek/[id]/_components/AddTimelineEntryForm.tsx
@@ -14,6 +14,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import WhatsAppNotifyToggle from "./WhatsAppNotifyToggle";
 
 interface AddTimelineEntryFormProps {
   serviceOrderId: string;
@@ -75,6 +76,7 @@ export default function AddTimelineEntryForm({
           </Button>
         </div>
       </div>
+      <WhatsAppNotifyToggle />
     </form>
   );
 }
