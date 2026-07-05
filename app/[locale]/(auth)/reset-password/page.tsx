18e6f0fa438af598fd048ccb673e0f193e0694ce
Haeryz <haeriz42069@gmail.com>
2026-07-05 22:48:53 +0700

    feat(auth): add reset password page with component integration

---
+++ app/[locale]/(auth)/reset-password/page.tsx
@@ -0,0 +1,11 @@
+import { ResetPasswordComponent } from "./components/ResetPasswordComponent";
+
+const ResetPasswordPage = async () => {
+  return (
+    <div className="flex flex-col w-full h-full items-center justify-center p-10">
+      <ResetPasswordComponent />
+    </div>
+  );
+};
+
+export default ResetPasswordPage;
